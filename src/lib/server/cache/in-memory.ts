e7760c7b1d36c8477270dd769b2a192bda14347e
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2025-02-13 17:47:37 +0100

    use `@heyputer/kv.js` for in-memory cache, simplify and test

--- src/lib/server/cache/in-memory.ts
+++ src/lib/server/cache/in-memory.ts
@@ -1,10 +1,9 @@
+import kvjs from '@heyputer/kv.js';
 import { createJitter, JitterMode, type JitterFn } from '../../utils/misc.js';
+import { addPrefix, stripPrefix } from '../../utils/string.js';
 import { BaseCache } from './base.js';
 
-type ValueWrapper<T> = { value: T; expire: number | undefined };
-
 type InMemoryCacheOptions = {
-	maxItems?: number;
 	keyPrefix?: string;
 	defaultTTL?: number;
 	defaultJitter?: JitterMode | JitterFn;
@@ -13,17 +12,17 @@ type InMemoryCacheOptions = {
 /** Simple cache with TTL and cap to maximum items stored. */
 export class InMemoryCache<V> extends BaseCache<V> {
 	readonly #options: InMemoryCacheOptions;
-	readonly #inner: Map<string, ValueWrapper<V>> = new Map();
+	readonly #inner: kvjs;
 
 	public static init<V>(options: InMemoryCacheOptions): InMemoryCache<V> {
 		return new this<V>(options);
 	}
 
-	private constructor(options: InMemoryCacheOptions, map?: Map<string, ValueWrapper<V>>) {
+	private constructor(options: InMemoryCacheOptions, store?: kvjs) {
 		super();
 
 		this.#options = Object.freeze({ ...options });
-		this.#inner = map ?? new Map();
+		this.#inner = store ?? new kvjs();
 	}
 
 	public child<V2 extends V>(
@@ -34,24 +33,14 @@ export class InMemoryCache<V> extends BaseCache<V> {
 			{
 				...this.#options,
 				...options,
-				keyPrefix: this.#key(keyPrefix),
+				keyPrefix: addPrefix(this.#options.keyPrefix, keyPrefix),
 			},
-			this.#inner as Map<`${typeof keyPrefix}${string}`, ValueWrapper<V2>>,
+			this.#inner,
 		);
 	}
 
 	public async get(key: string): Promise<V | undefined> {
-		const cached = this.#inner.get(this.#key(key));
-		if (typeof cached === 'undefined') {
-			return undefined;
-		}
-		if (!InMemoryCache.#isValid(cached)) {
-			this.#inner.delete(this.#key(key));
-
-			return undefined;
-		}
-
-		return cached.value;
+		return this.#inner.get(addPrefix(this.#options.keyPrefix, key)) as V | undefined;
 	}
 
 	public async set(
@@ -61,112 +50,34 @@ export class InMemoryCache<V> extends BaseCache<V> {
 		jitter?: JitterMode | JitterFn | undefined,
 	): Promise<void> {
 		ttl ??= this.#options.defaultTTL;
-		const jitterFn = createJitter(jitter ?? this.#options.defaultJitter ?? JitterMode.None);
-		this.#inner.set(this.#key(key), {
+		if (ttl === undefined) {
+			this.#inner.set(addPrefix(this.#options.keyPrefix, key), value);
+
+			return;
+		}
+
+		this.#inner.setex(
+			addPrefix(this.#options.keyPrefix, key),
 			value,
-			expire: typeof ttl === 'number' ? Date.now() + jitterFn(ttl * 1000) : undefined,
-		});
-
-		this.#housekeeping();
-	}
-
-	public async delete(key: string): Promise<void> {
-		this.#inner.delete(this.#key(key));
-	}
-
-	public async *keys(prefix?: string): AsyncGenerator<string, void, never> {
-		for (const key of this.#inner.keys()) {
-			const strippedKey = this.#stripPrefix(key);
-			if (typeof strippedKey !== 'undefined' && strippedKey.startsWith(prefix ?? '')) {
-				yield strippedKey;
-			}
-		}
-	}
-
-	public async clear(prefix?: string): Promise<void> {
-		if (typeof prefix !== 'undefined') {
-			this.#processBatch(this.#inner.entries(), (key) => key.startsWith(prefix));
-		} else if (typeof this.#options.keyPrefix !== 'undefined') {
-			this.#processBatch(this.#inner.entries(), () => true);
-		} else {
-			this.#inner.clear();
-		}
-	}
-
-	/**
-	 * Check if a cached value has expired.
-	 *
-	 * @param cached Cached value.
-	 * @param now Point-in-time to evaluate expiration against.
-	 */
-	static #isValid(cached: ValueWrapper<unknown>, now?: number): boolean {
-		if (typeof cached.expire !== 'number') {
-			return true;
-		}
-
-		return cached.expire >= (now ?? Date.now());
-	}
-
-	/**
-	 * Run housekeeping tasks on cache instance.
-	 *
-	 * Expired items will be removed, and if the cache is over capacity,
-	 * excess items will be randomly evicted in an attempt to cut size down.
-	 *
-	 * @param batchSize Number of cache items to evaluate at every tick. Keep this low to avoid locking for too long.
-	 */
-	#housekeeping(batchSize = 1000): void {
-		const now = Date.now();
-		const dropProbability =
-			typeof this.#options.maxItems !== 'undefined'
-				? Math.max(
-						0,
-						[...this.#inner.keys()].filter((key) => typeof this.#stripPrefix(key) !== 'undefined').length / // Number of items in this cache.
-							this.#options.maxItems -
-							1,
-					)
-				: 0;
-
-		setImmediate(() =>
-			this.#processBatch(
-				this.#inner.entries(),
-				(_, cached) => !InMemoryCache.#isValid(cached, now) || (dropProbability > 0 && Math.random() < dropProbability),
-				batchSize,
-			),
+			createJitter(jitter ?? this.#options.defaultJitter ?? JitterMode.None)(ttl * 1000),
 		);
 	}
 
-	#key(key: string | undefined) {
-		return (this.#options.keyPrefix ?? '') + (key ?? '');
+	public async delete(key: string): Promise<void> {
+		this.#inner.del(addPrefix(this.#options.keyPrefix, key));
 	}
 
-	#stripPrefix(key: string) {
-		const prefix = this.#options.keyPrefix ?? '';
-		if (!key.startsWith(prefix)) {
-			return undefined;
-		}
-
-		return key.substring(prefix.length);
+	public async *keys(prefix?: string): AsyncGenerator<string, void, never> {
+		yield* this.#inner
+			.keys(addPrefix(this.#options.keyPrefix, `${prefix ?? ''}*`))
+			.map((key) => stripPrefix(this.#options.keyPrefix, key)!);
 	}
 
-	#processBatch(
-		iterator: IterableIterator<[string, ValueWrapper<V>]>,
-		filter: (key: string, cached: ValueWrapper<V>) => boolean,
-		batchSize = 1000,
-	): void {
-		for (let i = 0; i < batchSize; i++) {
-			const next = iterator.next();
-			if (next.done === true) {
-				return;
-			}
+	public clear(prefix?: string): Promise<void> {
+		return this.clearPattern((prefix ?? '') + '*');
+	}
 
-			const [key, cached] = next.value;
-			const strippedKey = this.#stripPrefix(key);
-			if (typeof strippedKey !== 'undefined' && filter(strippedKey, cached)) {
-				this.#inner.delete(key);
-			}
-		}
-
-		setImmediate(() => this.#processBatch(iterator, filter, batchSize));
+	public async clearPattern(pattern: string): Promise<void> {
+		this.#inner.del(...this.#inner.keys(addPrefix(this.#options.keyPrefix, pattern)));
 	}
 }
---
+++ tests/server/cache/base.test.ts
@@ -0,0 +1,41 @@
+import { BaseCache } from '$lib/server/cache/base';
+import { InMemoryCache } from '$lib/server/cache/in-memory';
+import { beforeEach, describe, expect, it } from 'vitest';
+
+describe(BaseCache.name, () => {
+	describe('remember', () => {
+		const cache = InMemoryCache.init({});
+		beforeEach(async () => {
+			await cache.clear();
+			await cache.set('foo', 'bar');
+		});
+
+		it('should return the pre-existing value', async () => {
+			await expect(
+				cache.remember('foo', () => {
+					expect.unreachable();
+				}),
+			).resolves.equals('bar');
+		});
+
+		it('should generate and remember a missing value', async () => {
+			await expect(cache.remember('bar', async () => 'new value')).resolves.equals('new value');
+			await expect(cache.get('bar')).resolves.equals('new value');
+			await expect(Array.fromAsync(cache.keys())).resolves.to.has.members(['foo', 'bar']);
+		});
+
+		it('should generate and remember a missing value', async () => {
+			await expect(cache.remember('bar', async () => undefined)).resolves.toBeUndefined();
+			await expect(cache.get('bar')).resolves.toBeUndefined();
+			await expect(Array.fromAsync(cache.keys())).resolves.to.has.members(['foo']);
+		});
+
+		it('should re-throw any errors thrown by the callback', async () => {
+			const reason = new Error('rejected because reasons');
+
+			await expect(cache.remember('bar', () => Promise.reject(reason))).rejects.toThrow(reason);
+			await expect(cache.get('bar')).resolves.toBeUndefined();
+			await expect(Array.fromAsync(cache.keys())).resolves.to.has.members(['foo']);
+		});
+	});
+});
