8cc1a0df82ddd1cd21485bef841e75efd2962ea2
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2026-07-21 07:52:18 +0200

    Replace `$app/environment` import with `esm-env`

--- packages/utils/src/lib/logger.ts
+++ packages/utils/src/lib/logger.ts
@@ -1,8 +1,8 @@
 import * as Pino from 'pino';
-import { dev } from '$app/environment';
+import { DEV } from 'esm-env';
 
 const pino: typeof Pino.pino = typeof Pino === 'function' ? Pino : Pino.default;
 export const logger: Pino.Logger = pino({
-	transport: dev ? { target: 'pino-pretty' } : undefined,
-	level: dev ? 'debug' : 'info',
+	transport: DEV ? { target: 'pino-pretty' } : undefined,
+	level: DEV ? 'debug' : 'info',
 });
